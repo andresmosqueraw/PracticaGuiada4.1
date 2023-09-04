fa85ac7cec78f99a2545c2e15eea38898a22489f
Andrés Mosquera <85971409+andresmosqueraw@users.noreply.github.com>
2023-09-04 11:36:49 -0500

    update archivo.java

--- app/src/main/java/co/edu/uniempresarial/practicaguiada41/Archivo.java
+++ app/src/main/java/co/edu/uniempresarial/practicaguiada41/Archivo.java
@@ -17,61 +17,65 @@ import androidx.core.content.ContextCompat;
 
 
 public class Archivo {
-    private Context context;
-    private Activity activity;
+    private final Context context;
+    private final Activity activity;
 
     public Archivo(Context context, Activity activity) {
         this.context = context;
         this.activity = activity;
     }
 
-    private void crearDirectorio(File file){
-        if (!file.exists()) file.mkdir();
-    }
-
     public void guardarArchivo(String nombreArchivo, String informacion) {
-        File directorio = determinarDirectorio();
+        File directorio = obtenerDirectorio();
 
-        if (directorio != null) {
+        if (directorio == null) {
+            return;
+        }
+
+        escribirArchivo(directorio, nombreArchivo, informacion);
+    }
+
+    private File obtenerDirectorio() {
+        if (!tienePermisoEscrituraSD()) {
+            solicitarPermisoEscritura();
+            return null;
+        }
+
+        return crearODeterminarDirectorio();
+    }
+
+    private void escribirArchivo(File directorio, String nombreArchivo, String informacion) {
+        try {
+            File file = new File(directorio, nombreArchivo);
+            FileWriter writer = new FileWriter(file);
+            writer.append(informacion);
+            writer.flush();
+            writer.close();
+
             Toast.makeText(context, "Archivo guardado en: " + directorio, Toast.LENGTH_LONG).show();
-
-            try {
-                File file = new File(directorio, nombreArchivo);
-                FileWriter writer = new FileWriter(file);
-                writer.append(informacion);
-                writer.flush();
-                writer.close();
-            } catch (Exception e) {
-                Log.i("Archivo", e.getMessage());
-                Toast.makeText(context, "Error al guardar el archivo", Toast.LENGTH_LONG).show();
-            }
+        } catch (Exception e) {
+            Log.i("Archivo", e.getMessage());
+            Toast.makeText(context, "Error al guardar el archivo", Toast.LENGTH_LONG).show();
         }
     }
 
-    private File determinarDirectorio() {
-        File directorio;
-        solicitarPermisoExternal();
-        if (statusPermisoSD()) {
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                directorio = new File(Environment.getExternalStorageDirectory(), "ArchivoUE");
-            } else {
-                directorio = new File(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "ArchivoUE");
-            }
-        } else {
-            directorio = null;
+    private File crearODeterminarDirectorio() {
+        File directorio = (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P)
+                ? new File(Environment.getExternalStorageDirectory(), "ArchivoUE")
+                : new File(context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "ArchivoUE");
+
+        if (!directorio.exists()) {
+            directorio.mkdir();
         }
 
-        crearDirectorio(directorio);
         return directorio;
     }
 
-    private void solicitarPermisoExternal() {
-        if (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        }
+    private void solicitarPermisoEscritura() {
+        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
     }
 
-    private boolean statusPermisoSD() {
+    private boolean tienePermisoEscrituraSD() {
         return ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
     }
 }
